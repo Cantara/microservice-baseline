ab285630b7373ad66cd8c73929f4b0a6fcf6c76f
totto <totto@totto.org>
2016-09-17 22:00:03 +0200

    some more work

--- src/main/java/no/cantara/commands/config/ConstantValue.java
+++ src/main/java/no/cantara/commands/config/ConstantValue.java
@@ -2,9 +2,7 @@ package no.cantara.commands.config;
 
 public class ConstantValue {
 
-    public static final String HTML_CONTENT_TYPE = "text/html; charset=utf-8";
     public static final int COMMAND_TIMEOUT = 10000;
-    public static final String JSON_DATA = "jsondata";
 
     public static final String ATOKEN = "AsT5OjbzRn430zqMLgV3Ia";
     public static final String UTOKEN = "usT5OjbzRn430zqMLgV3Ia";
