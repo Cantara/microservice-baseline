18bbb012fa23fd6bed12a57bb704708fca80aeba
totto <totto@totto.org>
2017-12-21 19:03:51 +0100

    more housecleaning

--- src/main/java/no/cantara/service/basicauthapplication/ApplicationResource.java
+++ src/main/java/no/cantara/service/basicauthapplication/ApplicationResource.java
@@ -7,6 +7,7 @@ import com.jayway.jsonpath.JsonPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.ws.rs.*;
